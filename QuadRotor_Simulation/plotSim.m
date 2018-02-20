aed7bd1e39b40f01a88b0d59efcf393eb58efe91
Zachary Bluestein <bluestein@wustl.edu>
2018-02-19 20:20:44 -0600

    Replacing with Sydney's files

--- QuadRotor_Simulation/plotSim.m
+++ QuadRotor_Simulation/plotSim.m
@@ -1,4 +1,4 @@
-function plotSim(t,x_sim,y_sim,z_sim,phi_sim,theta_sim,psi_sim, target_x, target_y)
+function plotSim(t,x_sim,y_sim,z_sim,phi_sim,theta_sim,psi_sim)
 
 dt = t(2) - t(1);
 
@@ -11,15 +11,14 @@ z_min = min(z_sim);
 z_max = max(z_sim);
 
 
-for i = 1:5:length(t)
+for i = 1:length(t)
     plotQuadrotor(x_sim(i),y_sim(i),z_sim(i),phi_sim(i),theta_sim(i),psi_sim(i),5,t(i));
-    plotTarget(target_x(i), target_y(i), pi/4); 
     grid on;
     mytitle = sprintf('Time = %0.2f', t(i));
     title(mytitle);
     
-    lowerlim = min(1,i+200);
-    upperlim = max(length(t),i-200);
+    lowerlim = max(1,i-200);
+    upperlim = min(length(t),i+200);
     
     x_min = min(x_sim(lowerlim:upperlim));
     y_min = min(y_sim(lowerlim:upperlim));
