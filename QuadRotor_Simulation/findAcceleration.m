b9f8aa9eb0ad1e636961094318b9b4ff254669cd
Zachary Bluestein <bluestein@wustl.edu>
2018-02-22 18:03:21 -0600

    Modified logic of acceleration calculation

--- QuadRotor_Simulation/findAcceleration.m
+++ QuadRotor_Simulation/findAcceleration.m
@@ -1,6 +1,8 @@
-function quad_a = findAcceleration(drone_x0, drone_v0, target_x0, target_v0, x_max) 
+function [quad_a,time] = findAcceleration(drone_x0, drone_v0, target_x0, target_v0, x_max) 
 dX = drone_x0 - target_x0; 
 dV = drone_v0 - target_v0;
+
+
 
 % constraint: sqrt positive
 % positions are the same 
@@ -10,9 +12,20 @@ dV = drone_v0 - target_v0;
         %xf = x(1);
         a = x(2); 
         deltaX = dX;
-        deltaV = dV;
-        t = (-deltaV + sqrt((deltaV)^2 - 2*a*deltaX))/a;
-        obj = t + abs(1/dX * ((drone_v0 - dV * sqrt(dV^2 - 2*a*dX) - target_v0)));
+        if dX == 0 
+            obj = abs((drone_v0 - dV * sqrt(dV^2 - 2*a*dX) - target_v0));
+        else
+            if a == 0
+                a = 0.01;
+            end
+            deltaV = dV;
+            t = (-deltaV - sqrt((deltaV)^2 - 2*a*deltaX))/a;
+            if t < 0
+                t = (-deltaV + sqrt((deltaV)^2 - 2*a*deltaX))/a;
+            end
+            obj = t + abs(10/dX * ((drone_v0 - dV * sqrt(dV^2 - 2*a*dX) - target_v0)));
+        end
+        
     end
 
     function [c,ceq] = mycon(x)
@@ -27,7 +40,9 @@ g = 9.8;
 ub = [x_max, 3*g]; 
 lb = [0, -3*g]; 
 
-quad_result = fmincon(@fun,[1,1],[],[],[],[],lb,ub,@mycon);
+[quad_result, time] = fmincon(@fun,[0,0],[],[],[],[],lb,ub,@mycon);
+
+% need to establish logic for an impossible/illogical result 
 
 quad_a = quad_result(2); 
 
